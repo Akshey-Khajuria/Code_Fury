e87825ccc2691e847c736bb259ef7188043a0a7b
Akshey-Khajuria <akshey.vilas.khajuria.2000@gmail.com>
2025-11-24 23:40:12 +0530

    chore: initialize monorepo project structure

---
+++ Backend/src/test/java/com/codefury/backend/codefury_backend/CodefuryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codefury.backend.codefury_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodefuryBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
